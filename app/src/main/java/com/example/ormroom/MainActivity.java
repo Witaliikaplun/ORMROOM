5cd20603f1e9c8b7340fda79c8bc56999bf306ee
Witaliikaplun <vitalijkaplun@gmail.com>
2020-08-07 08:28:57 +0300

    3

--- app/src/main/java/com/example/ormroom/MainActivity.java
+++ app/src/main/java/com/example/ormroom/MainActivity.java
@@ -90,78 +90,66 @@ public class MainActivity extends AppCompatActivity {
 
 
         btnSaveAllRoom.setOnClickListener(view -> {
-            Single<Bundle> singleSaveAllRoom = Single.create(new SingleOnSubscribe<Bundle>() {
-                @Override
-                public void subscribe(@NonNull SingleEmitter<Bundle> emitter) throws
-                        Exception {
-                    String curLogin = "";
-                    String curUserID = "";
-                    String curAvatarUrl = "";
-                    Date first = new Date();
-                    List<RoomModel> roomModelList = new ArrayList<>();
-                    RoomModel roomModel = new RoomModel();
-                    for (RetrofitModel curItem : modelList) { //проходимся по листу, что нам достался от retrofit
-                        curLogin = curItem.getLogin();
-                        curUserID = String.valueOf(curItem.getId());
-                        curAvatarUrl = curItem.getAvatarUrl();
-                        roomModel.setLogin(curLogin);
-                        roomModel.setAvatarUrl(curAvatarUrl);
-                        roomModel.setUserId(curUserID);
-                        roomModelList.add(roomModel);
-                        OrmApp.get().getDB().productDao().insertAll(roomModelList);
-                    }
-                    Date second = new Date();
-                    Bundle bundle = new Bundle();
-                    List<RoomModel> tempList =
-                            OrmApp.get().getDB().productDao().getAll();
-                    bundle.putInt("count", tempList.size());
-                    bundle.putLong("msek", second.getTime() - first.getTime());
-                    emitter.onSuccess(bundle);
+            Single<Bundle> singleSaveAllRoom = Single.create((SingleOnSubscribe<Bundle>) emitter -> {
+                String curLogin = "";
+                String curUserID = "";
+                String curAvatarUrl = "";
+                Date first = new Date();
+                List<RoomModel> roomModelList = new ArrayList<>();
+                RoomModel roomModel = new RoomModel();
+                for (RetrofitModel curItem : modelList) { //проходимся по листу, что нам достался от retrofit
+                    curLogin = curItem.getLogin();
+                    curUserID = String.valueOf(curItem.getId());
+                    curAvatarUrl = curItem.getAvatarUrl();
+                    roomModel.setLogin(curLogin);
+                    roomModel.setAvatarUrl(curAvatarUrl);
+                    roomModel.setUserId(curUserID);
+                    roomModelList.add(roomModel);
+                    OrmApp.get().getDB().productDao().insertAll(roomModelList);
                 }
+                Date second = new Date();
+                Bundle bundle = new Bundle();
+                List<RoomModel> tempList =
+                        OrmApp.get().getDB().productDao().getAll();
+                bundle.putInt("count", tempList.size());
+                bundle.putLong("msek", second.getTime() - first.getTime());
+                emitter.onSuccess(bundle);
             }).subscribeOn(Schedulers.io())
                     .observeOn(AndroidSchedulers.mainThread());
             singleSaveAllRoom.subscribeWith(createObserver());
         });
 
         btnSelectAllRoom.setOnClickListener(view -> {
-        Single<Bundle> singleSelectAllRoom = Single.create(new SingleOnSubscribe<Bundle>() {
-            @Override
-            public void subscribe(@NonNull SingleEmitter<Bundle> emitter) throws
-                    Exception {
-                try {
-                    Date first = new Date();
-                    List<RoomModel> products =
-                            OrmApp.get().getDB().productDao().getAll();
-                    Date second = new Date();
-                    Bundle bundle = new Bundle();
-                    bundle.putInt("count", products.size());
-                    bundle.putLong("msek", second.getTime() - first.getTime());
-                    emitter.onSuccess(bundle);
-                } catch (Exception e) {
-                    emitter.onError(e);
-                }
+        Single<Bundle> singleSelectAllRoom = Single.create((SingleOnSubscribe<Bundle>) emitter -> {
+            try {
+                Date first = new Date();
+                List<RoomModel> products =
+                        OrmApp.get().getDB().productDao().getAll();
+                Date second = new Date();
+                Bundle bundle = new Bundle();
+                bundle.putInt("count", products.size());
+                bundle.putLong("msek", second.getTime() - first.getTime());
+                emitter.onSuccess(bundle);
+            } catch (Exception e) {
+                emitter.onError(e);
             }
         }).subscribeOn(Schedulers.io())
                 .observeOn(AndroidSchedulers.mainThread());
         singleSelectAllRoom.subscribeWith(createObserver());
     });
         btnDeleteAllRoom.setOnClickListener(view -> {
-            Single<Bundle> singleDeleteAllRoom = Single.create(new SingleOnSubscribe<Bundle>() {
-        @Override
-        public void subscribe(@NonNull SingleEmitter<Bundle> emitter) throws
-                Exception {
-            try {
-                Date first = new Date();
-                OrmApp.get().getDB().productDao().deleteAll();
-                Date second = new Date();
-                Bundle bundle = new Bundle();
-                bundle.putLong("msek", second.getTime() - first.getTime());
-                emitter.onSuccess(bundle);
-            } catch (Exception e) {
-                emitter.onError(e);
-            }
-        }
-    }).subscribeOn(Schedulers.io())
+            Single<Bundle> singleDeleteAllRoom = Single.create((SingleOnSubscribe<Bundle>) emitter -> {
+                try {
+                    Date first = new Date();
+                    OrmApp.get().getDB().productDao().deleteAll();
+                    Date second = new Date();
+                    Bundle bundle = new Bundle();
+                    bundle.putLong("msek", second.getTime() - first.getTime());
+                    emitter.onSuccess(bundle);
+                } catch (Exception e) {
+                    emitter.onError(e);
+                }
+            }).subscribeOn(Schedulers.io())
             .observeOn(AndroidSchedulers.mainThread());
     singleDeleteAllRoom.subscribeWith(createObserver());
         });
