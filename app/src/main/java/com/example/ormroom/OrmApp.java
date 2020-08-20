6a809d49023e2505d8c0dc0515a8493064bac112
Witaliikaplun <vitalijkaplun@gmail.com>
2020-08-20 12:25:01 +0300

    добавил библиотеку Stetho

--- app/src/main/java/com/example/ormroom/OrmApp.java
+++ app/src/main/java/com/example/ormroom/OrmApp.java
@@ -4,6 +4,8 @@ import android.app.Application;
 
 import androidx.room.Room;
 
+import com.facebook.stetho.Stetho;
+
 public class OrmApp extends Application {
     private static AppComponent component;
 
@@ -18,6 +20,22 @@ public class OrmApp extends Application {
         INSTANCE = this;
 
         component = DaggerAppComponent.create();
+
+
+
+
+        //Stetho-----------------------------------------------------------
+        Stetho.InitializerBuilder initializerBuilder =
+                Stetho.newInitializerBuilder(this);
+        initializerBuilder.enableWebKitInspector(
+                Stetho.defaultInspectorModulesProvider(this)
+        );
+        initializerBuilder.enableDumpapp(
+                Stetho.defaultDumperPluginsProvider(this)
+        );
+        Stetho.Initializer initializer = initializerBuilder.build();
+        Stetho.initialize(initializer);
+        //------------------------------------------------------------------
     }
     public UserGitDatabase getDB() {
         return database;
