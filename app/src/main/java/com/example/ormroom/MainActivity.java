fff7e293afc498b11fa608cae32f7dd1a646994f
Witaliikaplun <vitalijkaplun@gmail.com>
2020-08-21 15:09:19 +0300

    добавил библиотеку debugdrawer

--- app/src/main/java/com/example/ormroom/MainActivity.java
+++ app/src/main/java/com/example/ormroom/MainActivity.java
@@ -2,6 +2,8 @@ package com.example.ormroom;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.Application;
 import android.content.Context;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
@@ -12,25 +14,56 @@ import android.widget.Button;
 import android.widget.ProgressBar;
 import android.widget.TextView;
 import android.widget.Toast;
+//import android.widget.Toolbar;
+import androidx.appcompat.widget.Toolbar;
 
+
+import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Date;
 import java.util.List;
+import java.util.concurrent.TimeUnit;
+import java.util.logging.Logger;
 
 import javax.inject.Inject;
 
+
+
+
+import io.palaima.debugdrawer.actions.ActionsModule;
+import io.palaima.debugdrawer.actions.ButtonAction;
+import io.palaima.debugdrawer.actions.SpinnerAction;
+import io.palaima.debugdrawer.actions.SwitchAction;
+import io.palaima.debugdrawer.commons.BuildModule;
+import io.palaima.debugdrawer.commons.DeviceModule;
+import io.palaima.debugdrawer.commons.NetworkModule;
+import io.palaima.debugdrawer.commons.SettingsModule;
+import io.palaima.debugdrawer.fps.FpsModule;
+import io.palaima.debugdrawer.glide.GlideModule;
+import io.palaima.debugdrawer.location.LocationModule;
+import io.palaima.debugdrawer.logs.LogsModule;
+import io.palaima.debugdrawer.network.quality.NetworkQualityModule;
+import io.palaima.debugdrawer.okhttp3.OkHttp3Module;
+import io.palaima.debugdrawer.picasso.PicassoModule;
+import io.palaima.debugdrawer.scalpel.ScalpelModule;
+import io.palaima.debugdrawer.timber.TimberModule;
+
+import io.palaima.debugdrawer.view.DebugView;
 import io.reactivex.Single;
 import io.reactivex.SingleEmitter;
 import io.reactivex.SingleOnSubscribe;
 import io.reactivex.android.schedulers.AndroidSchedulers;
 import io.reactivex.observers.DisposableSingleObserver;
 import io.reactivex.schedulers.Schedulers;
+import jp.wasabeef.takt.Takt;
+import okhttp3.Cache;
+import okhttp3.OkHttpClient;
 import retrofit2.Call;
 import retrofit2.Callback;
 import retrofit2.Response;
-import retrofit2.Retrofit;
-import retrofit2.converter.gson.GsonConverterFactory;
+
 
 public class MainActivity extends AppCompatActivity {
     private TextView mInfoTextView;
@@ -47,6 +80,13 @@ public class MainActivity extends AppCompatActivity {
 
     @Inject
     Call <List<RetrofitModel>> call;
+
+
+    private Toolbar toolbar;
+
+    private DebugView debugView;
+    private OkHttpClient okHttpClient;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -161,6 +201,100 @@ public class MainActivity extends AppCompatActivity {
     singleDeleteAllRoom.subscribeWith(createObserver());
         });
 
+        setupToolBar();
+
+        SwitchAction switchAction = new SwitchAction("Test switch", new SwitchAction.Listener() {
+            @Override
+            public void onCheckedChanged(boolean value) {
+                Toast.makeText(MainActivity.this, "Switch checked", Toast.LENGTH_LONG).show();
+            }
+        });
+
+        ButtonAction buttonAction = new ButtonAction("Test button", new ButtonAction.Listener() {
+            @Override
+            public void onClick() {
+                Toast.makeText(MainActivity.this, "Button clicked", Toast.LENGTH_LONG).show();
+            }
+        });
+
+        SpinnerAction<String> spinnerAction = new SpinnerAction<>(
+                Arrays.asList("First", "Second", "Third"),
+                new SpinnerAction.OnItemSelectedListener<String>() {
+                    @Override
+                    public void onItemSelected(String value) {
+                        Toast.makeText(MainActivity.this, "Spinner item selected - " + value, Toast.LENGTH_LONG).show();
+                    }
+                }
+        );
+
+        debugView = findViewById(R.id.debug_view);
+
+        debugView.modules(
+//                new ActionsModule(switchAction, buttonAction, spinnerAction),
+//                //new FpsModule(Takt.stock(getApplication())),
+//                //new PicassoModule(picasso),
+//                new LocationModule(),
+//                new LogsModule(),
+//                new ScalpelModule(this),
+//                new TimberModule(),
+//                new OkHttp3Module(okHttpClient),
+//                new NetworkQualityModule(this),
+//                new DeviceModule(),
+//                new BuildModule(),
+//                new NetworkModule(),
+                new SettingsModule()
+        );
+
+
+    }
+    @Override
+    protected void onResume() {
+        super.onResume();
+        debugView.onResume();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        debugView.onPause();
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        debugView.onStart();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        debugView.onStop();
+    }
+
+    private Toolbar setupToolBar() {
+        toolbar = (Toolbar) findViewById(R.id.mainToolbar);
+        if (toolbar != null) {
+            setSupportActionBar(toolbar);
+        }
+        return toolbar;
+    }
+
+    private static final int DISK_CACHE_SIZE = 20 * 1024 * 1024; // 20 MB
+
+    private static OkHttpClient.Builder createOkHttpClientBuilder(Application app) {
+        // Install an HTTP cache in the application cache directory.
+        File cacheDir = new File(app.getCacheDir(), "okhttp3");
+        Cache cache = new Cache(cacheDir, DISK_CACHE_SIZE);
+
+        return new OkHttpClient.Builder()
+                .cache(cache)
+                .addInterceptor(LogsModule.chuckInterceptor(app))
+                .addInterceptor(NetworkQualityModule.interceptor(app))
+                .readTimeout(10, TimeUnit.SECONDS)
+                .writeTimeout(10, TimeUnit.SECONDS)
+                .connectTimeout(10, TimeUnit.SECONDS);
+
+
     }
 
     private void downloadOneUrl(Call <List<RetrofitModel>> call) throws IOException {
@@ -225,6 +359,8 @@ public class MainActivity extends AppCompatActivity {
             }
         };
     }
+
+
 }
 
 
